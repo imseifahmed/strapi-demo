deab8c829189c0f1c2002c1ef69a69f6bd030476
Seif Ahmed <seifahmeed9@gmail.com>
2025-09-08 23:53:05 +0300

    feat(components): add banner, link and logo-link component schemas

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,50 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface LayoutBanner extends Struct.ComponentSchema {
+  collectionName: 'components_layout_banners';
+  info: {
+    displayName: 'Banner';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    isVisible: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    link: Schema.Attribute.Component<'shared.link', false>;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    isButtonLink: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<['PRIMARY', 'SECONDARY']>;
+  };
+}
+
+export interface SharedLogoLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_logo_links';
+  info: {
+    displayName: 'Logo Link';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.banner': LayoutBanner;
+      'shared.link': SharedLink;
+      'shared.logo-link': SharedLogoLink;
+    }
+  }
+}
